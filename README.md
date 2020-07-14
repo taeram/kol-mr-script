eadb3f2685e606de0351e9fd995b61dbae386f1a
Jesse Patching <jesse.patching@ucalgary.ca>
2020-07-14 10:03:06 -0600

    Initial setup with typescript + webpack

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# kol-mr-script
-Kingdom of Loathing - Rewrite of the Mr. Script
+# Mr. Script
+
+A rewrite of [Hellion's Mr. Script](http://forums.kingdomofloathing.com/vb/showthread.php?t=182885), updating it for 
+the current version of Kingdom of Loathing (KoL).
+
+This will be a "slow" rewrite, focusing on the UI improvements first, and then on things like quests.
+
+# Build
+Run `npm run-script build`.
+
+## Notes
+Uses [userscript-typescript-webpack by vannhi](https://github.com/vannhi/userscript-typescript-webpack).
---
+++ webpack.config.ts
@@ -0,0 +1,52 @@
+import webpack from "webpack";
+import path from "path";
+import fs from "fs";
+import TerserPlugin from 'terser-webpack-plugin';
+
+const config: webpack.Configuration = {
+  entry: "./src/index.ts",
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: /(\s@|UserScript==)/i,
+          },
+        }
+      })
+    ]
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true
+        }
+      }
+    }]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "MrScript.user.js"
+  },
+  resolve: {
+    modules: [
+      "node_modules",
+      "src"
+    ],
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: fs.readFileSync(path.resolve(__dirname, "src/index.ts"), "utf-8").replace(/(==\/UserScript==)[\s\S]+$/, "$1"),
+      entryOnly: true,
+      raw: true
+    })
+  ]
+};
+
+export default config;
